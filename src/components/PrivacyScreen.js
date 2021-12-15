8cd8d1f4261fad16fb004a41ee60515b065e717c
zliu <60162477+NickyL279@users.noreply.github.com>
2021-12-15 15:48:35 -0500

    Add files via upload

---
+++ src/components/PrivacyScreen.js
@@ -0,0 +1,124 @@
+const PrivacyScreen = () => {
+    return (
+        <>
+            <div className="text-white">
+                <h2>Privacy Policy</h2><br/>
+                <h5>Last Updated, Dec 15, 2021.</h5>
+                <span className="fs-5">
+                    We know that you care how information about you is used and
+                    shared, and we appreciate your trust that we will do so carefully and sensibly.
+                    This Privacy Policy will not intentionally disclose any contents that relate to
+                    your personal privacy to any third party unless otherwise
+                    stipulated herein.
+                </span>
+            </div>
+
+            <div className="text-white">
+                <h3>What Personal Information About Users Does IMDb Clone Collect?</h3><br/>
+                <span className="fs-5">Search for movie info in IMDB Clone</span><br/><br/>
+                <span className="fs-5">View, or use content in IMDB Clone
+                </span><br/><br/>
+                <span className="fs-5">Provide information for your user IMDB Clone account
+                </span><br/><br/>
+                <span className="fs-5">Communicate with us through email
+                </span><br/><br/>
+            </div>
+
+            <div className="text-white">
+                <h3 className="">For What Purposes Does IMDb Clone Use Your Personal Information?</h3>
+                <br/>
+                <span className="fs-5">1. <i>Personal Information</i> – We only collect your email for the registration purpose.</span><br/><br/>
+                <span className="fs-5">2. <i>Non-Personal Information.</i> We may use Non-Personal Information for the
+                    following
+                    purposes:
+                </span><br/><br/>
+                <span className="fs-5">(1)</span>
+                <span className="fs-5"><i>For Personalized User Experience</i> – We
+                may use your
+                Non-Personal Information to understand your user characteristics, and other trends
+                related to user’s
+                habits;</span><br/><br/>
+                <span className="fs-5">(2)</span>
+                <span className="fs-5"><i>To Help Improve Our Service</i> – We may
+                use Non-Personal
+                Information to provide, maintain, enhance and improve our applications, services and
+                websites.
+            </span><br/><br/>
+                <span className="fs-5">(3)</span> <span className="fs-5"><i>For the Further Development</i> of this
+                website – We might use
+                Non-Personal Information for development purposes only.</span>
+            </div>
+            <br/><br/>
+            <div className="text-white">
+                <h3 className="">The Possibility of Personal Information Being Disclosed</h3>
+                <br/>
+                <span className="fs-5">1. <i>Personal Information.</i> We do not store Personal Information, so your
+                    Personal
+                    Information cannot be disclosed by us.</span><br/><br/>
+                <span className="fs-5">2. <i>Non-Personal Information.</i> We will not match non-personal information
+                    and personal
+                    information (such as numbering your name and your user-specific device).</span>
+            </div>
+            <br/><br/>
+            <div className="text-white">
+                <h3 className=""><u><i>Users Sharing Your Information</i></u></h3>
+                <br/>
+                <span className="fs-5"><u><i>We can not control the behavior of other users you share your data with. We
+                    cannot and will not control the behaviors of other users that you share
+                    information with when you use an application, service or website (including
+                    online forum), or how such users will use or share such information. We will not
+                    be responsible for any behavior or conduct of third party circumventing our
+                    security measures.
+                </i></u></span>
+            </div>
+            <br/><br/>
+            <div className="text-white">
+                <div className="">How Secure Is Information About Me?</div>
+                <br/>
+                <span className="fs-5">We are concerned about keeping confidential of your information. We do not
+                    collect Personal
+                    Information, and we have adopted effective measures to protect your Non-Personal
+                    Information, so
+                    that such information will not be accessed and used without authorization.</span>
+                <span className="fs-5"><u><i>Please
+                    note that although we have taken sufficient security measures to protect your
+                    information, there is
+                    not absolute guarantee that we can prevent non-personal information from being
+                    accessed or used
+                    without authorization.</i></u></span>
+            </div>
+            <br/><br/>
+            <div className="text-white">
+                <h3 className="">Conditions of Use, Notices, and Revisions</h3>
+                <span className="fs-5">If you choose to use our website, your use and any dispute over privacy is
+                    subject to this Notice and our Conditions of Use, including limitations on
+                    damages, resolution of disputes, and application of the law of the state of
+                    Massachusetts.</span><br/><br/>
+                <span className="fs-5">If you have any concerns about privacy, please send us a thorough description to
+                    our customer service team, and we will get back to you within 2 business days
+                    .</span><br/><br/>
+                <span className="fs-5">Our business changes constantly, and our Privacy Notice may change as well. We
+                    will send out email notification for any change we just make in the terms and
+                    conditions. Unless stated otherwise, our current Privacy Notice applies to all
+                    information that we have about you and your account. We stand behind the
+                    promises we make, however, and will never materially change our policies and
+                    practices to make them less protective of customer information collected in the
+                    past without the consent of affected customers.</span><br/><br/>
+            </div>
+            <br/><br/>
+
+            <div className="text-white">
+                <h3 className="">Contacts</h3>
+                <br/>
+                <span className="fs-5">If you have any questions or suggestions with respect to this Privacy Policy or
+                    our privacy
+                    system, or if you want to report any violations of this Privacy Policy or abuse
+                    of the
+                    application, service or website, please contact us at: liuzc20@gmail.com</span>
+            </div>
+            <br/><br/>
+        </>
+    )
+}
+
+export default PrivacyScreen
